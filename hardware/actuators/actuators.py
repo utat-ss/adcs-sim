e418c9ac4c9b167afb2eee52961028a0eb204187
Jenny6182 <jen.hwang@mail.utoronto.ca>
2026-06-05 15:57:38 -0400

    added virtual actuator abstract class and virtualmtq subclass

--- hardware/actuators/actuators.py
+++ hardware/actuators/actuators.py
@@ -1,10 +1,29 @@
 # Virtual actuator models for use as hardware stubs in simulation
 
 from pathlib import Path
-
+from abc import ABC, abstractmethod
 import numpy as np
 
-class VirtualCMG:
+class VirtualActuator(ABC):
+    def __init__(self, cfg_file: Path):
+        # common fields
+        self.cfg_file = cfg_file
+        self.model: str = ""
+
+    def load_cfg(self):
+        """
+        Optional shared helper for standardizez config loading (?)
+        """
+        raise NotImplementedError("Each sensor can override this if needed")
+
+    @abstractmethod
+    def command(input: np.ndarray):
+        """
+        Every actuator must implement its own command function.
+        Output format is sensor-specific.
+        """
+
+class VirtualCMG(VirtualActuator):
     """
     Generic virtual Control Moment Gyroscope model for use in simulation
     """
@@ -24,8 +43,12 @@ class VirtualCMG:
         # TODO: Config file format
         with open(cfg_file, 'r') as f:
             raise NotImplementedError()
+        
+    def command(self, input: np.ndarray) -> np.ndarray:
+        """
+        """
 
-class VirtualRWL:
+class VirtualRWL(VirtualActuator):
     """
     Generic virtual Reaction Wheel model for use in simulation
     """
@@ -45,4 +68,18 @@ class VirtualRWL:
         # TODO: Config file format
         with open(cfg_file, 'r') as f:
             raise NotImplementedError()
+    
+    def command(self, input: np.ndarray) -> np.ndarray:
+        """
+        """
+
+
+class VirtualMTQ(VirtualActuator):
+    """
+    Generic magnetorquer class.
+    """
+
+    def command(self, input: np.ndarray) -> np.ndarray:
+        """Input current, output magnetic field / torque vector in sensor frame"""
+
 
