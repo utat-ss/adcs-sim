14e106433915fe5883106fb0fea27ccc2b7f84f0
Jenny6182 <jen.hwang@mail.utoronto.ca>
2026-06-30 15:24:40 -0400

    added units to constants

--- constants.py
+++ constants.py
@@ -2,5 +2,6 @@
 constants.py contains all the commonly used constants throughout the software
 """
 
-G = 6.6743 * 10**-11 # Gravitational Constant
-M = 5.97219 * 10**24 # Earth's Mass
+G = 6.6743 * 10**-11 # m^3 / (kg * s^2)  --- Gravitational Constant
+M = 5.97219 * 10**24 # kg   ---   Earth's Mass
+mu = G*M # m^3 / s^2   ---   G*M,the gravitational parameter
