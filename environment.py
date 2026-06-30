84923deb64bde94f1028c6c7b4f71f5ed0e9e17b
declanwhchan <declan8.chan@gmail.com>
2026-06-30 14:34:51 -0400

    ADD: atmospheric density calculation based on orbital state

--- environment.py
+++ environment.py
@@ -6,7 +6,8 @@ def j2_acceleration_m_s2(
     r_eq_m: float = 6378136.3,
     j2: float = 1.08262668e-3,
 ) -> tuple[float, float, float]:
-    """Calculate the J2 perturbative acceleration.
+    """
+    Calculate the J2 perturbative acceleration.
 
     Arguments:
     r_eci_m (tuple): x, y, z -> Earth-Centered Inertial position vector [meters].
@@ -31,3 +32,35 @@ def j2_acceleration_m_s2(
     az = factor * z * (3 - 5 * z2_over_r2)
 
     return (ax, ay, az)
+
+
+def atmospheric_density_kg_m3(
+    r_eci_m: tuple[float, float, float],
+    r_eq_m: float = 6378136.3,
+) -> float:
+    """
+    Calculate atmospheric density based on orbital state.
+
+    Arguments:
+    r_eci_m (tuple): x, y, z Earth-Centered Inertial position vector [m].
+    r_eq_m (float): Mean equatorial radius of Earth [m].
+
+    Returns:
+    float: Atmospheric density [kg/m^3].
+    """
+    r_norm_m = np.linalg.norm(r_eci_m)
+
+    if r_norm_m == 0:
+        raise ValueError("Position vector must be nonzero.")
+
+    altitude_m = r_norm_m - r_eq_m
+
+    if altitude_m < 0:
+        raise ValueError("Altitude must be nonnegative.")
+
+    sea_level_atmosphere_kg_m3 = 1.225
+    atmosphere_scale_m = 8500.0
+
+    density_kg_m3 = sea_level_atmosphere_kg_m3 * np.exp(-altitude_m / atmosphere_scale_m)
+
+    return float(density_kg_m3)
