21f1554d2edbdc4142c0ec5e7b2730aba3f53c1b
Mr.M <73618961+Mr-Msf@users.noreply.github.com>
2026-06-30 14:37:03 -0400

    ADD: Eclipse detection functionality to FSS class

--- hardware/sensors/sensors.py
+++ hardware/sensors/sensors.py
@@ -60,7 +60,7 @@ class VirtualFSS(VirtualSensor):
         self.model = str(cfg.get("model", self.model))
         self.fov_rad = float(cfg["fov_hcone_deg"])
         self.rate_hz = float(cfg["rate_hz"])
-        self.cov_deg2 = np.zeros(cfg["cov_deg2"], dtype=float)
+        self.cov_deg2 = np.asarray(cfg["cov_deg2"], dtype=float)
 
     def _angle_computation(self, incident_light_sensor: np.ndarray):
         """
@@ -124,8 +124,27 @@ class VirtualFSS(VirtualSensor):
             "beta_deg": beta_deg,
             "sun_present": inside_fov,
         }
+    
+    def eval_eclipse(self, sat_vector: np.ndarray, sun_vector: np.ndarray, earth_vector: np.ndarray) -> bool:
+        """to be added"""
 
-    def measure(self, sun_vec_body, R_BS, r_mount=None):
+        plane_normal = np.cross(sun_vector, sat_vector)
+        proj_earth_info = utils.sphere_plane_intersection(earth_vector, 6378137.0, plane_normal) # change to reference to constants file
+        
+        sun_vect_planar = utils.to_planar_vector(sat_vector, sun_vector)
+        earth_vect_planar = utils.to_planar_vector(sat_vector, proj_earth_info[0])
+
+        tangent_vects_planar = utils.determine_body_tangent_vectors(earth_vect_planar, proj_earth_info[1])
+
+        sun_tangent_angles = (utils.angle_between_vectors(sun_vect_planar, tangent_vects_planar[0]), utils.angle_between_vectors(sun_vect_planar, tangent_vects_planar[1]))
+        tangent_tangent_angle = utils.angle_between_vectors(tangent_vects_planar[0], tangent_vects_planar[1])
+
+        if round(tangent_tangent_angle, 10) == round(sun_tangent_angles[0] + sun_tangent_angles[1], 10):
+            return True
+        else:
+            return False
+
+    def measure(self, sun_vec_body, R_BS, sun_vector, earth_vector, r_mount=None):
         """
         Compute FSS angular output from Sun and satellite position vectors.
 
@@ -148,6 +167,8 @@ class VirtualFSS(VirtualSensor):
         Returns:
             Same dict as measure_from_sensor_vector.
         """
+
+        # following to be refactored:
         if r_mount is None:
             r_mount = np.zeros(3)
         else:
@@ -163,6 +184,13 @@ class VirtualFSS(VirtualSensor):
 
         incident_light_body = sun_vec_body - r_mount
         incident_light_sensor = R_BS @ incident_light_body
+
+        if self.eval_eclipse(np.array([1,0,0]), sun_vector, earth_vector):
+            return {
+            "alpha_deg": 0.0,
+            "beta_deg": 0.0,
+            "sun_present": False,
+            }
 
         return self._angle_computation(incident_light_sensor)
 
@@ -271,65 +299,6 @@ class VirtualSTR(VirtualSensor):
 
         return np.array([xyz[0], xyz[1], xyz[2], w], dtype=float)
 
-    def determine_body_tangent_vectors(self, planar_body_vector: np.ndarray, body_radius: float) -> tuple:
-        """to be added"""
-
-        normed_vec = np.linalg.norm(planar_body_vector)
-        tangent_dot_body = normed_vec ** 2 - (body_radius ** 2)
-        if tangent_dot_body <= 0.0:
-            raise ValueError("planar_body_vector must be of a greater magnitude than body_radius.")
-        
-        temp = math.sqrt(tangent_dot_body) * body_radius
-        
-        tangent_1_coord_1 = (tangent_dot_body * planar_body_vector[0] + planar_body_vector[1] * temp) / (normed_vec ** 2)
-        tangent_1_coord_2 = (tangent_dot_body * planar_body_vector[1] - planar_body_vector[0] * temp) / (normed_vec ** 2)
-
-        tangent_2_coord_1 = (tangent_dot_body * planar_body_vector[0] - planar_body_vector[1] * temp) / (normed_vec ** 2)
-        tangent_2_coord_2 = (tangent_dot_body * planar_body_vector[1] + planar_body_vector[0] * temp) / (normed_vec ** 2)
-
-        return (np.array([tangent_1_coord_1, tangent_1_coord_2]), np.array([tangent_2_coord_1, tangent_2_coord_2]))
-
-    def _to_planar_vector(self, sat_vector: np.ndarray, body_vector: np.ndarray) -> np.ndarray:
-        """to be added"""
-        
-        coord_1 = np.dot(sat_vector, body_vector) / np.linalg.norm(sat_vector)
-        temp = (np.linalg.norm(body_vector) ** 2) - (coord_1 ** 2)
-        if temp < 0.0:
-            temp = round(temp, 10)
-            if temp < 0.0:
-                raise ValueError("Invalid vectors entered into function.")
-            
-        coord_2 = math.sqrt(temp)
-
-        return np.array([coord_1, coord_2])
-    
-    def _from_planar_vector(self, sat_vector: np.ndarray, body_vector_3D: np.ndarray, body_vector_2D: np.ndarray) -> np.ndarray:
-        """to be added"""
-        
-        plane_normal = np.cross(body_vector_3D, sat_vector)
-        dir_vect_2 = np.cross(sat_vector, plane_normal)
-
-        unit_vect_1 = sat_vector / np.linalg.norm(sat_vector)
-        if np.array_equal(dir_vect_2, np.zeros(3)):
-            unit_vect_2 = np.zeros(3)
-        else:
-            unit_vect_2 = dir_vect_2 / np.linalg.norm(dir_vect_2)
-
-        return unit_vect_1 * body_vector_2D[0] + unit_vect_2 * body_vector_2D[1]
-
-    def angle_between_vectors(self, vector_1: np.ndarray, vector_2: np.ndarray) -> float:
-        """to be added"""
-
-        dot_p = np.dot(vector_1, vector_2)
-        norm_1 = np.linalg.norm(vector_1)
-        norm_2 = np.linalg.norm(vector_2)
-        if norm_1 == 0.0 or norm_2 == 0.0:
-            raise ValueError("Both vector_1 and vector_2 must have nonzero magnitude.")
-
-        cos_theta = dot_p / (norm_1 * norm_2)
-        vector_angle_rad = math.acos(cos_theta)
-        return vector_angle_rad
-
     def eval_body_in_zone(self, sat_vector: np.ndarray, body_vector: np.ndarray, body_name: str) -> bool:
         """to be added"""
 
@@ -355,12 +324,12 @@ class VirtualSTR(VirtualSensor):
             zone_angle_rad = self.fov_rad / 2
             body_radius =  6378137.0 # equatorial radius (class may give false negative near poles)
 
-        body_angle = self.angle_between_vectors(sat_vector, body_vector)
+        body_angle = utils.angle_between_vectors(sat_vector, body_vector)
 
-        body_vector_2D = self._to_planar_vector(sat_vector, body_vector)
-        tangent_vectors_2D = self.determine_body_tangent_vectors(body_vector_2D, body_radius)
-        tangent_vectors_3D = (self._from_planar_vector(sat_vector, body_vector, tangent_vectors_2D[0]), self._from_planar_vector(sat_vector, body_vector, tangent_vectors_2D[1]))
-        tangent_angles = (self.angle_between_vectors(sat_vector, tangent_vectors_3D[0]), self.angle_between_vectors(sat_vector, tangent_vectors_3D[1]))
+        body_vector_2D = utils.to_planar_vector(sat_vector, body_vector)
+        tangent_vectors_2D = utils.determine_body_tangent_vectors(body_vector_2D, body_radius)
+        tangent_vectors_3D = (utils.from_planar_vector(sat_vector, body_vector, tangent_vectors_2D[0]), utils.from_planar_vector(sat_vector, body_vector, tangent_vectors_2D[1]))
+        tangent_angles = (utils.angle_between_vectors(sat_vector, tangent_vectors_3D[0]), utils.angle_between_vectors(sat_vector, tangent_vectors_3D[1]))
 
         if body_angle <= zone_angle_rad:
             # print("0")
