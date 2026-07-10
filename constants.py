9d9a6257a61c60cd28c9f06a6df452594c80a5d2
Jenny6182 <jen.hwang@mail.utoronto.ca>
2026-07-10 17:22:30 -0400

    CHG: changed the constants to match capitalized and units format, as well as removed e

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@
 constants.py contains all the commonly used constants throughout the software
 """
 
-G = 6.6743 * 10**-11 # m^3 / (kg * s^2)  --- Gravitational Constant
-M = 5.97219 * 10**24 # kg   ---   Earth's Mass
-mu = G*M # m^3 / s^2   ---   G*M,the gravitational parameter
+G_m3_kgs2 = 6.6743 * 10**-11 # m^3 / (kg * s^2)  --- Gravitational Constant
+M_kg = 5.97219 * 10**24 # kg   ---   Earth's Mass
+EARTH_MU_m3_s2 = G_m3_kgs2*M_kg # m^3 / s^2   ---   G*M,the gravitational parameter
