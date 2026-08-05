b90e98ed9caac5dba2c730a585611ecd6b0370a5
Mikael <73618961+Mr-Msf@users.noreply.github.com>
2026-08-05 15:57:44 -0400

    FIX: Typo in constants.py

--- constants.py
+++ constants.py
@@ -11,4 +11,4 @@ SUN_RADIUS_m = 695700000.0 # m --- average radius of the sun
 EARTH_RADIUS_EQ_m = 6378137.0 # m --- equatorial radius of the Earth (semi-major axis of WGS 84 ellipsoid)
 EARTH_RADIUS_POLAR_m = 6356752.314245 # m --- radius of the Earth at poles (semi-minor axis of WGS 84 ellipsoid)
 
-MOON_RADIUS_m = 1737400.0, # m --- average radius of the Moon
+MOON_RADIUS_m = 1737400.0 # m --- average radius of the Moon
