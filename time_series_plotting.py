f3e21e909fdb99aece9c7e5ecbc76ae4a6f53082
SRuhil <120292312+SRuhil@users.noreply.github.com>
2026-08-07 11:12:46 -0400

    ADD: basic one element time series plotting

---
+++ time_series_plotting.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+# get the time series data passed as arguments
+# TODO: check if the data is in the correct format, if not, convert it to the correct format
+# TODO: check attitude and orbit modules
+"""
+plot_orbit(t: List[float], kep: List[KeplerianElements])
+
+Absent the actual simulation runtime to generate the data, I would just manually 
+create some short timeseries that have the right data shape in order to test that everything looks as you expect. 
+The physics aren't important to the plotting so just some random sine waves or something would be fine.
+"""
+
+# plotting
+# x-axis: time ... what representation? system time? or just seconds since the program started
+
+# y-axis: always a float
+
+
+def plot_time_series(t, y, ylabel, title: str):
+    """
+    Plot a time series of data.
+    
+    Parameters:
+    t (list): A list of time values.
+    y (list): A list of corresponding data values.
+    ylabel (str): The label for the y-axis.
+    title (str): The title of the plot.
+    """
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(t, y)
+    plt.xlabel('Time (s)')
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
