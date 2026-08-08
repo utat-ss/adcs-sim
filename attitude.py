146613faa63aec9e73ed56bc147dbed596d9e999
harrywangsw <68875077+harrywangsw@users.noreply.github.com>
2026-08-08 09:28:19 -0400

    ADDED: Function that caculates the linearized rotational dynamics

--- attitude.py
+++ attitude.py
@@ -100,3 +100,98 @@ def dynamics(t, x, J, torque_ext):
         [eta_dot],
         omega_dot
     ])
+
+
+def linearized_attitude_matrices(q_0, omega_0, J):
+    """
+    Compute A, B for the linearized reduced-quaternion attitude system.
+    Linearized system:
+
+    Arguments:
+        q_0:
+            Reduced quaternion vector part at the linearization point.
+            Shape: (3,)
+
+        omega_0:
+            Body-frame angular velocity at the linearization point.
+            Shape: (3,)
+
+        J:
+            Spacecraft inertia matrix.
+            Shape: (3, 3)
+
+    Returns:
+        A:
+            Linearized state matrix.
+            Shape: (6, 6)
+
+        B:
+            Linearized input matrix.
+            Shape: (6, 3)
+    """
+    J_inv = np.linalg.inv(J)
+
+    q1, q2, q3 = q_0
+    omega1, omega2, omega3 = omega_0
+
+    A_omega_omega = J_inv@(skew(J @ omega_0)-skew(omega_0)@J)
+
+    A_omega_q = np.zeros((3, 3))
+    
+    f = np.sqrt(1 - q1**2 - q2**2 - q3**2)
+
+    dg_domega = 0.5 * np.array([
+        [f, -q3, q2],
+        [q3, f, -q1],
+        [-q2, q1, f],
+    ])
+    
+    df_dq1 = -q1/f
+    df_dq2 = -q2/f
+    df_dq3 = -q3/f
+    
+    dg_dq = 0.5 * np.array([
+        [
+            df_dq1 * omega1,
+            df_dq2 * omega1 + omega3,
+            df_dq3 * omega1 - omega2,
+        ],
+        [
+            df_dq1 * omega2 - omega3,
+            df_dq2 * omega2,
+            df_dq3 * omega2 + omega1,
+        ],
+        [
+            df_dq1 * omega3 + omega2,
+            df_dq2 * omega3 - omega1,
+            df_dq3 * omega3,
+        ],
+    ])
+
+    A = np.block([
+        [A_omega_omega, A_omega_q],
+        [dg_domega,     dg_dq],
+    ])
+
+    B = np.vstack([
+        J_inv,
+        np.zeros((3, 3)),
+    ])
+
+    return A, B
+
+
+# if __name__ == "__main__":
+
+#     J = np.diag([0.02, 0.025, 0.03])
+
+#     q_0 = np.array([0.0, 0.0, 0.0])
+#     omega_0 = np.array([0.0, 0.0, 0.0])
+
+#     A, B = linearized_attitude_matrices(q_0, omega_0, J)
+
+#     print("A =")
+#     print(A)
+
+#     print("\nB =")
+#     print(B)
