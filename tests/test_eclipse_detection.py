e6a813c201560e99f353726f6eae1322135791a2
SRuhil <120292312+SRuhil@users.noreply.github.com>
2026-08-23 21:24:11 -0400

    ADD: tests for find_config_path function

---
+++ tests/test_eclipse_detection.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+from hardware.adcs import ADCS
+
+# Tests for _find_config_path method of ADCS class
+def test_find_config_path_existing_model(tmp_path, monkeypatch):
+    icd_file = tmp_path / "test_model.json" # temporary file to simulate the ICD file
+    icd_file.write_text("{}")
+
+    monkeypatch.setattr(
+        "your_module.SENSOR_ICD_DIR",
+        tmp_path,
+    )
+
+    adcs = ADCS("test", np.eye(3))
+
+    result = adcs._find_config_path("Test-Model")
+    assert result == icd_file
+
+
+def test_find_config_path_not_existing_model():
+    adcs  = ADCS("test", np.eye(3))
+
+    with pytest.raises(FileNotFoundError):
+        adcs._find_config_path("does-not-exist-model")
+
