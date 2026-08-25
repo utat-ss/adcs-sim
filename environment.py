d0ce83c880c9b3ff266dc8a43de34086261f6aee
Mikael <73618961+Mr-Msf@users.noreply.github.com>
2026-08-25 15:33:14 -0400

    FIX: environment.py bugs and consistency

--- environment.py
+++ environment.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 import numpy as np
 import pymsis
+from utils import conversions as conv
+import constants as const
 
 def j2_acceleration_m_s2(r_eci_m: tuple[float, float, float]) -> tuple[float, float, float]:
     """
@@ -15,8 +17,8 @@ def j2_acceleration_m_s2(r_eci_m: tuple[float, float, float]) -> tuple[float, fl
     """
     x, y, z = r_eci_m
     r_norm_m = np.linalg.norm(r_eci_m)
-    mu_m3_s2 = 3.986004418e14
-    r_eq_m = 6378136.3
+    mu_m3_s2 = const.EARTH_MU_m3_s2
+    r_eq_m = const.EARTH_RADIUS_EQ_m
     j2 = 1.08262668e-3
 
     if r_norm_m < r_eq_m + 100000:
@@ -31,8 +33,7 @@ def j2_acceleration_m_s2(r_eci_m: tuple[float, float, float]) -> tuple[float, fl
 
     return (ax, ay, az)
 
-
-def atmospheric_density_kg_m3(
+def NRLMSIS_atmospheric_density_kg_m3(
     r_eci_m: tuple[float, float, float],
     epoch: datetime,
     f107: float = 150.0,
@@ -52,28 +53,25 @@ def atmospheric_density_kg_m3(
     Returns:
     float: Atmospheric mass density [kg/m^3].
     """
-    x, y, z = r_eci_m
+    r_eci_m = np.asarray(r_eci_m, dtype=float)
     r_norm_m = np.linalg.norm(r_eci_m)
-    r_eq_m = 6378136.3
-    altitude_m = r_norm_m - r_eq_m
 
-    if r_norm_m == 0.0:
-        raise ValueError("Position vector must be nonzero.")
+    if len(r_eci_m) != 3:
+        raise ValueError("r_eci_m must be a tuple of length 3.")
+    
+    if type(epoch) != datetime:
+        raise ValueError("epoch must be of type datetime.")
 
-    if altitude_m < 0.0:
-        raise ValueError("Altitude must be nonnegative.")
+    if r_norm_m < const.EARTH_RADIUS_POLAR_m:
+        raise ValueError("Position vector norm must not be less than the Earth's radius.")
 
-    longitude_deg = np.degrees(np.arctan2(y, x))
-    latitude_deg = np.degrees(
-        np.arctan2(z, np.sqrt(x * x + y * y))
-    )
+    lla_coords = conv.eci_to_lla(r_eci_m, epoch)
 
     dates = np.array([epoch], dtype="datetime64[us]")
-    lons = np.array([longitude_deg])
-    lats = np.array([latitude_deg])
-    alts = np.array([altitude_m / 1000.0])
+    lons = np.array([lla_coords[0]])
+    lats = np.array([lla_coords[1]])
+    alts = np.array([lla_coords[2] / 1000.0])
 
-    # pymsis returns densities in cm^-3
     output = pymsis.calculate(
         dates,
         lons,
@@ -85,9 +83,14 @@ def atmospheric_density_kg_m3(
         version=2.1,
     )
 
-    total_mass_density_g_cm3 = output[0, 0, 0, 0]
+    total_mass_density_kg_m3 = output[0][0]
 
-    return float(total_mass_density_g_cm3 * 1000.0)
+    return float(total_mass_density_kg_m3)
+
+def calc_atm_velocity_m_s():
+    """
+    """
+    pass
 
 def aerodynamic_drag_perturbation_m_s2(
     velocity_m_s: np.ndarray,
