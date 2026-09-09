a0201c2a233fb166515b6004c5a15fc54319a681
Mikael Sharify-funk <mikaelsharifyfunk@gmail.com>
2026-09-09 17:25:43 -0400

    FIX: Coordinate transformation bug

--- temp_attitude_propagator.py
+++ temp_attitude_propagator.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import math
 
 from hardware.sensors import sensors
-from utils import conversions as conv, geometric_calculations as gc
+from utils import conversions as conv, geometric_calculations as gc, quaternion_math as quat
 import constants as const
 from hardware import adcs
 
@@ -12,14 +12,14 @@ def get_meas_vect(alpha, beta, DCM):
     alpha_vec = np.array([0,1,0]) * math.tan(np.deg2rad(alpha))
     beta_vec = np.array([0,0,1]) * math.tan(np.deg2rad(beta))
     vect = unit_x + alpha_vec + beta_vec
-    DCMinv = np.linalg.inv(DCM)
-    return DCMinv @ vect
+    # DCMinv = np.linalg.inv(DCM)
+    return DCM @ vect
 
 def get_res(): # np.identity(3),conv.rot_z(-math.pi/2), , 
-    DCMs = [conv.rot_z(math.pi/2)]#, conv.rot_z(math.pi), conv.rot_y(math.pi/2), conv.rot_y(-math.pi/2)]
-    print(DCMs)
+    DCMs = [np.identity(3), conv.rot_z(math.pi/2)]#, conv.rot_z(math.pi), conv.rot_y(math.pi/2), conv.rot_y(-math.pi/2)]
+    # print(DCMs)
     R = const.EARTH_RADIUS_EQ_m + 500000
-    EARTH_TO_SUN_VEC = np.array([0., 150*10**9, 0.])
+    EARTH_TO_SUN_VEC = np.array([10**7, 5*10**7, 0.])
 
     angles = np.linspace(0, 2*math.pi, 100)
 
@@ -27,10 +27,10 @@ def get_res(): # np.identity(3),conv.rot_z(-math.pi/2), ,
 
     positions = []
     for angle in angles:
-        positions.append([R * math.cos(angle), R * math.sin(angle), 0])
+        positions.append([R * math.cos(-angle), R * math.sin(-angle), 0])
 
     positions = np.asarray(positions, dtype=float)
-    attitude_q = np.array([0,0,0,1])
+    attitude_q = np.array([0,0,math.sin(math.pi/8),math.cos(math.pi/8)])
 
     res = []
 
@@ -38,11 +38,12 @@ def get_res(): # np.identity(3),conv.rot_z(-math.pi/2), ,
     for pos in positions:
         earth_vec = -pos
         sun_vec = earth_vec + EARTH_TO_SUN_VEC
+        # print(sun_vec)
         processed_meas = []
         for DCM in DCMs:
             meas = FSS.measure(attitude_q, sun_vec, ADC.eval_eclipse_heuristic(sun_vec, earth_vec)[0], DCM)
-            print(ADC.eval_eclipse_heuristic(sun_vec, earth_vec)[0], meas)
-            vect = get_meas_vect(meas["alpha_deg"], meas["beta_deg"], DCM)
+            # print(ADC.eval_eclipse_heuristic(sun_vec, earth_vec)[0], meas)
+            vect = get_meas_vect(meas["alpha_deg"], meas["beta_deg"], conv.quat_to_rotmat(attitude_q) @ DCM )
             processed_meas.append(gc.angle_between_vectors(sun_vec, vect))
         res.append(processed_meas)
 
@@ -50,7 +51,7 @@ def get_res(): # np.identity(3),conv.rot_z(-math.pi/2), ,
 
     return angles, res
 
-get_res()
+# get_res()
 
 # NEED TO FIX ERROR IN ROTMAT->QUAT
 
